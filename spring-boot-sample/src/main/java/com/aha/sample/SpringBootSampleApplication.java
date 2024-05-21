1d1d2925f1b3f4127fa2d30099b67a595a07aaeb
error <15139705191@163.com>
2024-05-21 11:33:08 +0800

    [新增]：添加自己的测试模块

--- spring-boot-sample/src/main/java/com/aha/sample/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/aha/sample/SpringBootSampleApplication.java
@@ -1,13 +1,13 @@
-package com.aha.test;
+package com.aha.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMyTestApplication {
+public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMyTestApplication.class, args);
+		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 
 }
